0cff6e53a05b72cf01ab33cc4cd13524913f4c2b
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-08-29 22:20:30 +0000

    Main
    Name, BD, and phone numbers are appearing as shown on assignment page

--- main.cs
+++ main.cs
@@ -1,7 +1,33 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public class Person
+{
+    public string Name { get; set; }
+    public string BirthDate { get; set; }
+    public string WorkPhoneNumber { get; set; }
+    public string CellPhoneNumber { get; set; }
+}
+
+public static class PersonalInfo
+{
+    public static string Display(Person person)
+    {
+        return $"{person.Name}\n{person.BirthDate}\nwork {person.WorkPhoneNumber}\ncell {person.CellPhoneNumber}";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Sample data for demonstration
+        Person samplePerson = new Person 
+        {
+            Name = "Francisco Isaac",
+            BirthDate = "01/16/2002",
+            WorkPhoneNumber = "813-847-3797",
+            CellPhoneNumber = "813-847-3797"
+        };
+        Console.WriteLine(PersonalInfo.Display(samplePerson));
+    }
 }
