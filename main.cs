f0b31a73d9721a4b021fa64d27ede940da10f6aa
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-08-29 23:42:39 +0000

    Scrapped most of the code just to make it simple as i believe i ended up going to far and this ended up making the code way to complex for a first assignment turn in

--- main.cs
+++ main.cs
@@ -1,29 +1,9 @@
 using System;
 
-public class Person
-{
-    public string Name { get; set; }
-    public string BirthDate { get; set; }
-    public string WorkPhoneNumber { get; set; }
-    public string CellPhoneNumber { get; set; }
-
-    public override string ToString()
-    {
-        return $"{Name}\n{BirthDate}\nwork {WorkPhoneNumber}\ncell {CellPhoneNumber}";
-    }
-}
-
 public class Program
 {
     public static void Main()
     {
-        var samplePerson = new Person 
-        {
-            Name = "Lucinda Potter",
-            BirthDate = "6/24/1992",
-            WorkPhoneNumber = "000-000-0101",
-            CellPhoneNumber = "000-000-0189"
-        };
-        Console.WriteLine(samplePerson);
+        Console.WriteLine("Lucinda Potter\n6/24/1992\nwork 000-000-0101\ncell 000-000-0189");
     }
 }
